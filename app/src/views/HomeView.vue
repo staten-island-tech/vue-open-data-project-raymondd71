fe5eac864f460cab7f9c6dfbf19a7118c50512e9
raymondd71 <raymondd71@nycstudents.net>
2025-03-03 12:32:20 -0500

    Day1 cooked
    no progress
    wilsonis tocuing me

---
+++ app/src/views/HomeView.vue
@@ -0,0 +1,20 @@
+<script setup>
+import TheWelcome from '../components/TheWelcome.vue'
+
+import { ref, onMounted } from 'vue'
+const baby = ref('')
+async function getinfant() {
+  let res = await fetch('https://data.cityofnewyork.us/resource/fcau-jc6k.json')
+  let data = await res.json()
+  baby.value = data.results
+}
+
+onMounted(() => {
+  getPokemon()
+})
+</script>
+<template>
+  <main>
+    <TheWelcome />
+  </main>
+</template>
