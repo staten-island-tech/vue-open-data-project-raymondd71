bcdac8e1e869cd0ffc007b88d1b3ffd6316e361d
raymondd71 <raymondd71@nycstudents.net>
2025-04-03 11:41:40 -0400

    s

--- app/src/views/AboutView.vue
+++ app/src/views/AboutView.vue
@@ -1,22 +1,46 @@
 <template>
-  <div class="flex justify-around">
-    <BarChart class="w-[40%]" :key="chartKey" :data="chartData" />
-  </div>
+  <div><BarChart :data="chartData" /></div>
 </template>
 
-<script setup>
-import BarChart from '@/components/BarChart.vue'
+<script>
 import { reactive, onMounted, ref } from 'vue'
-const students = ref('')
+import BarChart from '@/components/BarChart.vue'
+
+const students = reactive([0, 0, 0])
+const chartKey = ref(0)
 async function studentresult() {
   let res = await fetch('https://data.cityofnewyork.us/resource/k8ah-28f4.json')
   let data = await res.json()
-  students.value = data
+  data.forEach((el) => {
+    students[0] += parseInt(el.count_of_students_in_hs)
+    if (el.count_of_testers === '0-5') {
+      students[1] += 5
+    } else {
+      students[1] += parseInt(el.count_of_testers)
+    }
+    if (el.number_of_offers === '0-5') {
+      students[2] += 5
+    } else {
+      students[2] += parseInt(el.number_of_offers)
+    }
+  })
+  students[0] -= students[1]
+  students[1] -= students[2]
+  console.log(students)
+  chartKey.value++
 }
 
 onMounted(() => {
   studentresult()
 })
+const chartData = {
+  labels: ['Non-Test Takers', 'Rejected Test Takers', 'Accepted Test Takers'],
+  datasets: [
+    {
+      label: 'Students',
+      backgroundColor: '#42A5F5',
+      data: [0, 0, 0],
+    },
+  ],
+}
 </script>
-
-<style scoped></style>
